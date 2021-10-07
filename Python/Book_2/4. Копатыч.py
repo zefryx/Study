980efb1cd0257d953ba2d1e212a8aca8771a9287
zefryx <74315242+zefryx@users.noreply.github.com>
2021-10-07 16:49:53 +0500

    Update 4. Копатыч.py
    В пайдроиде написал, есть в телефоне.

--- Python/Book_2/4. Копатыч.py
+++ Python/Book_2/4. Копатыч.py
@@ -1,21 +1,57 @@
+import random
 
-# Код для проверки, является ли строка числом
-def isDigit(s): #Проверяет является ли строка числом
-    startCode = ord('0') #Диапазон проверки для цифр от 0
-    stopCode = ord('9') # до 9
-    res = True #если False то не число
-    tka = False
+#расшифровка комманд
+def decode(s):
+	dcd = ''
+	for i in s:
+		if(ord(i) in codeList):
+			dcd += i
+	return dcd
 
-    p = 0 #счетчик while
-    while(p < len(s) and res):
-        if(ord(s[p]) == ord('.') and not tka):
-            tka = True
-        elif(ord(s[p]) < startCode or ord(s[p]) > stopCode):
-            res = False
-        p += 1
-    return res
+code = 'WSADE0123'
+codeList = []
+for i in range(len(code)):
+	codeList.append(ord(code[i]))
 
-x = '114.445.345'
-print(isDigit(x))
+command = input('Введите команды Копатыча: ')
+dCommand = ''
+dCommList = []
+clCommand = []
+txt = ''
+print()
 
+#создание трех отправленных команд
+for o in range(3):
+	
+	#создание помех в команде
+	i = 0
+	while (i < (len(command))):
+		if(random.randint(0, 0) < 1 and i < (len(command) - 1) ):
+			dCommand += command[i]
+			i += 1
+		for j in range(random.randint(1,5)):
+			dCommand += chr(random.randint(65, 122))
+		dCommand += command[i]
+		i += 1
+		
+	dCommList.append(dCommand)
+	print(f'Строка с помехами №{o}: {dCommand}')
+	dCommand = ''
+
+print()
+
+#расшифровка команды
+for i in range(len(dCommList)):
+	clCommand.append(decode(dCommList[i]))
+	print(f'Очищенная строка №{i}: {clCommand[i]}')
+
+#выбор команды для использования
+txt = clCommand[0] 
+
+for i in range(len(clCommand)-1):
+	i += 1
+	if(len(txt) > len(clCommand[i])):
+		txt = clCommand[i]
+		
+print(f'\nВыбранная для исполнения команда: ', txt)
 input()
