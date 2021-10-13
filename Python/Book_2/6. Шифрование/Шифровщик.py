2752cc046c0fd18127c422731ade0200bcb9eac8
zefryx <dimac.cyka@gmail.com>
2021-10-13 23:20:01 +0500

    Эммм

---
+++ Python/Book_2/6. Шифрование/Шифровщик.py
@@ -0,0 +1,93 @@
+from tkinter import *
+
+def goCode():
+    return 0
+
+def goDecode():
+    return 0
+
+def copyToClipboard():
+    root.clipboard_clear();
+    root.clipboard_append(tOutput.get(1.0, END))
+
+def pasteFromClipboard():
+    try:
+        tInput.insert(END, root.clipboard_get())
+    except:
+        tInput.insert(END, '\nОШИБКА: буфер пуст')
+
+def resToDef():
+    tInput.delete(1.0, END)
+    txt = tOutput.get(1.0, END)
+    txt = txt[0:len(txt) - 1]
+    tInput.insert(1.0, txt)
+
+def clearText():
+    tInput.delete(1.0, END)
+    tOutput.delete(1.0, END)
+
+def setMenuPos(event):
+    menuInput.post(event.x_root, event.y_root)
+
+#инициализация окна
+root = Tk()
+root.resizable(False, False)
+root.title('Шифровщик')
+
+WIDTH = 800
+HEIGHT = 320
+POS_X = root.winfo_screenwidth() // 2 - WIDTH // 2
+POS_Y = root.winfo_screenheight() // 2 - HEIGHT // 2
+root.geometry(f'{WIDTH}x{HEIGHT}+{POS_X}+{POS_Y}')
+
+#текстовые поля
+textInput = Label(text='Введите исходный текст:')
+textInput.place(x=2, y=1)
+textOutput = Label(text='Результат:')
+textOutput.place(x=2, y=157)
+
+tInput = Text(width=70, height=8, wrap=WORD)
+tInput.place(x=5, y=20)
+
+#скролл бары для текстовых полей
+scrollInput = Scrollbar(command = tInput.yview, width=20)
+scrollInput.place(x=570, y=20, height = 132)
+tInput['yscrollcommand'] = scrollInput.set
+
+tOutput = Text(width=70, height=8, wrap=WORD)
+tOutput.place(x=5, y=180)
+
+scrollOutput = Scrollbar(command = tOutput.yview, width=20)
+scrollOutput.place(x=570, y=180, height = 132)
+tOutput['yscrollcommand'] = scrollOutput.set
+
+#меню на правую кнопку
+menuInput = Menu(tearoff=False)
+menuInput.add_command(label='Копировать реультат', command=copyToClipboard)
+menuInput.add_command(label='Вставить в исходный текст', command=pasteFromClipboard)
+menuInput.add_command(label='Результат -> Исходный', command=resToDef)
+menuInput.add_command(label='Очистить  текст', command=clearText)
+tInput.bind('<Button-3>', setMenuPos)
+
+#кнопки шифровать / дешифровать
+btnCode = Button(text='Шифровать', width=25, command=goCode)
+btnCode.place(x=600, y=20)
+
+btnDecode = Button(text='Дешифровать', width=25, command=goDecode)
+btnDecode.place(x=600, y=50)
+
+#радиокнопки
+textAlgo = Label(text='Алгоритм:')
+textAlgo.place(x=600, y=100)
+rBtn = IntVar()
+rBtn.set(0)
+algo01 = Radiobutton(text='Инвентировать', variable=rBtn, value=0)
+algo02 = Radiobutton(text='Замена с соседней', variable=rBtn, value=1)
+algo03 = Radiobutton(text='+1', variable=rBtn, value=2)
+algo04 = Radiobutton(text='+позиция (до 33)', variable=rBtn, value=3)
+algo01.place(x=600, y=120)
+algo02.place(x=600, y=140)
+algo03.place(x=600, y=160)
+algo04.place(x=600, y=180)
+
+root.mainloop()
