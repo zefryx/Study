fc7a165c570a995623b573b5f2698c89259ee70a
zefryx <74315242+zefryx@users.noreply.github.com>
2021-10-14 09:58:30 +0500

    Update Сортировки.py

--- Python/Book_2/8. Сортировка/Сортировки.py
+++ Python/Book_2/8. Сортировка/Сортировки.py
@@ -1,1 +1,16 @@
-x = 1
+# сортировка пузырьком
+'''
+#a = [5, 4, 3, 2, 1]
+a = [19, 21, 30, 31, 40]
+
+print (a)
+
+for i in range(len(a) - 1):
+    print('i', i)
+    for j in range(len(a) - 1 - i):
+        print('j', j)
+	if (a[j] < a[j + 1]):
+	    a[j], a[j + 1] = a[j + 1], a[j]
+
+print (a)
+'''
